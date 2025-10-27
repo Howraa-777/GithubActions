3b2620abeb20d40a7f34463c99bef66bbbe1242e
Howraa Al-Robaie <alrobaiehowraa@gmail.com>
2025-10-27 09:33:36 -0500

    Implemented Power() and added passing tests (AlRobaie)

--- Console/Program.cs
+++ Console/Program.cs
@@ -86,6 +86,8 @@ public class Program
 	// Implement this method following a similar pattern as above
 	public static double Power(string x, string y)
 	{
-		return 0.0;
-	}
+        double baseVal = double.Parse(x);
+        double expVal = double.Parse(y);
+        return Math.Pow(baseVal, expVal);
+    }
 }
--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,7 +4,7 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_AlRobaie()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
@@ -12,7 +12,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_AlRobaie()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,10 +20,27 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_AlRobaie()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
+
+    [TestMethod]
+    public void Power_Valid_AlRobaie()
+    {
+        double expected = 8;
+        double result = Program.Power("2", "3");
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void Power_ZeroExponent_AlRobaie()
+    {
+        double expected = 1;
+        double result = Program.Power("7", "0");
+        Assert.AreEqual(expected, result);
+    }
+
 }
